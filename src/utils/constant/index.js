569bd7d96bee9df183a76451b542d421a8b9bc71
Roshnigupta518 <roshnigupta518@gmail.com>
2026-07-13 10:14:33 +0530

    change url and published apk

--- src/utils/constant/index.js
+++ src/utils/constant/index.js
@@ -1,6 +1,6 @@
 export const environment = {
-     baseUrl: 'http://139.5.6.137/RKSKAPI/api/',
-     imageUrl: 'http://139.5.6.137/RKSKAPI/',
+     baseUrl: 'https://rksk.nhmmp.gov.in/rkskapi/api/',
+     imageUrl: 'https://rksk.nhmmp.gov.in/rkskapi/api/',
     // baseUrl: 'http://139.5.6.137/RKSKUATAPI/api/',
     // imageUrl : 'http://139.5.6.137/RKSKUATAPI/',
     GOOGLE_API_KEY : 'AIzaSyARAwP9SIAcyIFHdrrYbXA8a7EJe5nGV0o'
